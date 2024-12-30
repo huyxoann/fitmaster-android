905eeedb21342b8cdde2153ee969e4a176e9031f
huyxoann <huytn.21it@vku.udn.vn>
2024-12-30 22:38:38 +0700

    add testing action

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,8 +59,11 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
     testImplementation(libs.junit)
+    testImplementation(libs.truth)
     androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.truth)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
