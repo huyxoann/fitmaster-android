89956d216601c949339cc31427d752390314b3b1
huyxoann <huytn.21it@vku.udn.vn>
2024-12-30 22:00:47 +0700

    change some thing

--- app/src/main/java/com/huyxoann/fitmaster/MainActivity.kt
+++ app/src/main/java/com/huyxoann/fitmaster/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             FitmasterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Huy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
